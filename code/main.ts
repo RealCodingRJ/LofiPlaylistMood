59c41b557a5ec07f8401da8f2823aa37f605e271
Ryan James <realcoder2023@gmail.com>
2025-06-07 23:55:32 -0400

    added a way to save links from file and learn them

--- code/main.ts
+++ code/main.ts
@@ -5,7 +5,7 @@ import { TITLE } from "./TITLES";
 
 function getData(data: string) {
   try {
-    fs.writeFile("lofiLinks.io", data, (err) => {
+    fs.appendFile("lofiLinks.io", data, (err) => {
       if (err) {
         throw new Error("Cannot run File");
       }
@@ -16,7 +16,19 @@ function getData(data: string) {
   }
 }
 
+function indexArrayLink(url, index) {
+  for (let i = 0; i < 5; i++) {
+    getData(url[i]);
+  }
+}
+
 console.log(TITLE);
 const link = readline.question("Enter Link from YouTube: ");
+const savedLink = window.open(link);
 
 console.log(getData(link));
+console.log(savedLink);
+
+for (let i = 0; i < 1; i++) {
+  indexArrayLink(link, i);
+}
